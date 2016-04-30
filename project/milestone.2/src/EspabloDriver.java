56ea4e45e289bad2209c33f0584cbccbfacb40a8
Weon Yuan <weon.yuan@gmail.com>
2016-04-30 17:24:48 -0400

    Fix string expressions in DFA. Update csvs.

--- project/milestone.2/src/EspabloDriver.java
+++ project/milestone.2/src/EspabloDriver.java
@@ -16,8 +16,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 
 import com.sun.istack.internal.logging.Logger;
